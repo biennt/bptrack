131ebf417460d16b11489f56b5a21a2a9bdfe598
Nguyen Thanh Bien <bien@bienlab.com>
2019-03-19 09:23:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -202,7 +202,7 @@ function get_graph($conn, $id) {
         echo "            ['Time', 'Systolic', 'Diastolic', 'Heart beat'],\n";
         ############################################################
         $sql = "SELECT * FROM bpmain where bpid='" . $id . "'" . " and recordtime between DATE_SUB(NOW(), INTERVAL 30 DAY) and NOW()";
-        echo $sql . "\n";
+        #echo $sql . "\n";
 	$result = $conn->query($sql);
         $numofrow=$result->num_rows;
         $rowcount=0;
