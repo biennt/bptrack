81c3c0e5b349d53d3d51677b26df3b79564da602
Nguyen Thanh Bien <bien@bienlab.com>
2019-03-21 07:59:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed, die: " . $conn->connect_error);
 }
-$conn->query('SET time_zone = Asia/Saigon');
+$conn->MySQLi->query("SET timezone = 'GMT+7'");
 
 if (isset($_POST['uname']) && isset($_POST['psw'])) {
 	$create_account = $_POST['create_account'];
