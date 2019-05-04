16dc18a08a7819fa2b33d8c12a0b19c3bf89708c
Nguyen Thanh Bien <bien@bienlab.com>
2019-05-04 15:58:53 +0700

    Update api.php

--- api.php
+++ api.php
@@ -9,7 +9,22 @@ class api extends restful_api {
 		if ($this->method == 'GET'){
 			// Hãy viết code xử lý LẤY dữ liệu ở đây
 			// trả về dữ liệu bằng cách gọi: $this->response(200, $data)
-			$data = "user";
+			$data = "";
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+    			die("Connection failed, die: " . $conn->connect_error);
+			}
+			$conn->query("SET time_zone = 'Asia/Saigon'");
+			$sql = "SELECT name, password FROM user where bpid=1";
+			$result = $conn->query($sql);
+			if ($numofrow == 1) {
+				$row = $result->fetch_assoc();
+				$password=$row['password'];
+				$username=$row['name'];
+				$arr = array('name' => $name, 'password' => $password);
+				$data = json_encode($arr);
+			}
 			$this->response(200, $data);
 		}
 		elseif ($this->method == 'POST'){
