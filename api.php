e682b19caa661ad90ff999b9b1778aff1d0bbebd
Nguyen Thanh Bien <bien@bienlab.com>
2019-05-04 16:11:35 +0700

    Update api.php

--- api.php
+++ api.php
@@ -9,22 +9,24 @@ class api extends restful_api {
 		if ($this->method == 'GET'){
 			// Hãy viết code xử lý LẤY dữ liệu ở đây
 			// trả về dữ liệu bằng cách gọi: $this->response(200, $data)
-			$data = "";
 			require_once 'dbconf.php';
 			$conn = new mysqli($servername, $username, $password, $dbname);
 			// Check connection
 			if ($conn->connect_error) {
-    			$data=("Connection failed, die: " . $servername);
-			}
-			$conn->query("SET time_zone = 'Asia/Saigon'");
-			$sql = "SELECT name, password FROM user where bpid=1";
-			$result = $conn->query($sql);
-			if ($numofrow == 1) {
-				$row = $result->fetch_assoc();
-				$password=$row['password'];
-				$username=$row['name'];
-				$arr = array('name' => $name, 'password' => $password);
-				$data = json_encode($arr);
+    				$data = "Connection failed, die with " . $servername;
+			} else {
+				$conn->query("SET time_zone = 'Asia/Saigon'");
+				$sql = "SELECT name, password FROM user where bpid=1";
+				$result = $conn->query($sql);
+				if ($numofrow == 1) {
+					$row = $result->fetch_assoc();
+					$password=$row['password'];
+					$username=$row['name'];
+					$arr = array('name' => $name, 'password' => $password);
+					$data = json_encode($arr);
+				} else {
+					$data = "query error";
+				}
 			}
 			$this->response(200, $data);
 		}
