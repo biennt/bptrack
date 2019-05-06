52c59295d8d65e09dbcaa1f61f879e512a741bfa
Nguyen Thanh Bien <bien@bienlab.com>
2019-05-06 15:16:31 +0700

    Update api.php

--- api.php
+++ api.php
@@ -16,7 +16,8 @@ class api extends restful_api {
     				$data = "Connection failed, die with " . $servername;
 			} else {
 				$conn->query("SET time_zone = 'Asia/Saigon'");
-				$uid = $this->$params[0];
+				$para = $this->$params;
+				$uid = $para[0];
 				$endpoint = $this->endpoint;
 				$data = "DEBUG params: " . $uid . " endpoint: " . $endpoint;
 				//if (ctype_alnum($uid)) {
